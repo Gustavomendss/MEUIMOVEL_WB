5cc475049cc72b131372f01c146763061db152f8
gustavomendss <gustavomendes95.gm@gmail.com>
2022-11-30 22:26:26 -0300

    MEUIMOVEL  - Finalizado !

--- MEUIMOVEL_WEB/core/models.py
+++ MEUIMOVEL_WEB/core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-
 # CRIANDO O  MODELO PARA O MEUIMOVEL
+
+class todo(models.Model):
+    name = models.TextField(max_length=255)
+    status = models.BooleanField(default=False)
 
 class Base(models.Model):
     criacao = models.DateTimeField(auto_created=True)
@@ -11,24 +14,24 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
-
 class MEUIMOVELapp(Base):
     título = models.CharField(max_length=255)
     url = models.URLField(unique=True)
+
 
     class Meta:
         verbose_name = 'MEUIMOVEL'
         verbose_name_plural = 'MEUIMOVEL'
 
+
     def __str__(self):
         return self.título
-
 
 # Criando a parte de avaliacao
 class Avaliacao(Base):
     MEUIMOVEL = models.ForeignKey(MEUIMOVELapp,
-                                  related_name='avaliacoes',
-                                  on_delete=models.CASCADE)
+                               related_name='avaliacoes',
+                               on_delete=models.CASCADE)
     nome = models.CharField(max_length=255)
     email = models.EmailField()
     comentario = models.TextField(blank=True, default='')
@@ -37,12 +40,8 @@ class Avaliacao(Base):
     class Meta:
         verbose_name = 'Avaliação'
         verbose_name_plural = 'Avaliações'
-        unique_together = ['email', 'MEUIMOVEL']
+        unique_together = ['email','MEUIMOVEL']
 
         def __str__(self):
             return f'{self.nome} avaliou o MEUIMOVEL {self.MEUIMOVEL} com a nota {self.avaliacao}'
 
-
-class todo(models.Model):
-    name = models.TextField(max_length=255)
-    status = models.BooleanField(default=False)
